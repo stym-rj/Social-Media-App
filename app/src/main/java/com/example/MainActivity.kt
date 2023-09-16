a123a3f2d8c7a79e2c8134d5b00854fbda1e13c0
satyam raj <satyam3279raj@gmail.com>
2023-09-17 02:35:59 +0530

    added bottom nav and related fragments

---
+++ app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.Fragments.FragmentHome
+import com.example.Fragments.FragmentPost
+import com.example.Fragments.FragmentProfile
+import com.example.Fragments.FragmentSearch
+import com.example.socialmediaapp.R
+import com.example.socialmediaapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.botNavHome -> setFragContainer(FragmentHome())
+                R.id.botNavSearch -> setFragContainer(FragmentSearch())
+                R.id.botNavPost -> setFragContainer(FragmentPost())
+                R.id.botNavProfile -> setFragContainer(FragmentProfile())
+                else -> setFragContainer(FragmentHome())
+            }
+            true
+        }
+    }
+
+    private fun setFragContainer(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.container.id, fragment)
+            .commit()
+    }
+}
