1752bd306a6a82274bef16def5e15d3955c887f0
satyam raj <satyam3279raj@gmail.com>
2023-09-16 22:17:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.socialmediaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialmediaapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
