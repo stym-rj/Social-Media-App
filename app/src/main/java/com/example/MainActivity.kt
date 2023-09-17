06a33d3e8f4c1396ce96abe259a23ab954ed6ce6
satyam raj <satyam3279raj@gmail.com>
2023-09-17 05:37:44 +0530

    added toolbar on top

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.Fragments.FragmentHome
 import com.example.Fragments.FragmentPost
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId) {
