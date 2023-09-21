3461c877d573e6ba7849b371f2e7f5392a136e2a
satyam raj <satyam3279raj@gmail.com>
2023-09-21 08:32:38 +0530

    added Logout functionality

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.fragments.FragmentHome
@@ -12,6 +15,7 @@ import com.example.fragments.FragmentProfile
 import com.example.fragments.FragmentSearch
 import com.example.socialmediaapp.R
 import com.example.socialmediaapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -19,6 +23,10 @@ class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +53,29 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+
+        binding.appToolbar.appIcon.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                // Create the dialog
+                val alertDialogBuilder = AlertDialog.Builder(this@MainActivity)
+                alertDialogBuilder.setTitle("Logout")
+                alertDialogBuilder.setMessage("Do you want really want to Logout?")
+                alertDialogBuilder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, _ ->
+                    // Code to execute when the "OK" button is clicked
+                    dialog.dismiss()
+                    auth.signOut()
+                    startActivity(Intent(this@MainActivity, AuthenticationActivity::class.java))
+                    finish()
+                })
+                alertDialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, _ ->
+                    // Code to execute when the "Cancel" button is clicked
+                    dialog.dismiss()
+                })
+
+                val alertDialog = alertDialogBuilder.create()
+                alertDialog.show()
+            }
+        })
     }
 
     private fun setFragContainer(fragment: Fragment) {
